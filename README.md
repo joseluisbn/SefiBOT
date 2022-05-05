7baf26756932675bf28fd30394b237d359c0f6e1
José Luis Blanco <joseluisbn@gmail.com>
2022-05-05 17:26:38 +0200

    first interactions, about message example

--- README.md
+++ README.md
@@ -4,20 +4,33 @@
 
 ## Install or adding bot to Discord
 
-Download SefiBOT from GitHub.
-
-Add the bot in https://discord.com/developers/applications
+- Download SefiBOT from GitHub.
+- Add the bot in https://discord.com/developers/applications
 
 ![imagen](https://user-images.githubusercontent.com/42966407/166497905-4acaac41-7597-4bf5-84b1-8f9608bfe4e8.png)
 
-Clic here and name it as SefiBOT
+Clic here and name it as SefiBOT (or whatever you like)
+
+- pip install pipenv
+- pipenv shell
+- pipenv discord.py
+- https://discord.com/developers/applications
+- Add application
+- Bot: add a bot
+- Token: Copy the token
+- OAuth2 > URL Generator > bot
+- Add permissions
+- Copy the URL and go to it
+- Add the bot to chosen server
 
 ## Commands
 
 ## Further reading
 
 - https://discord.com/developers/docs/intro
+- https://github.com/Rapptz/discord.py
+- https://discordpy.readthedocs.io/en/latest/#
 
 ## About
 
-Bot developed by BleizeN.
+SefiBOT v0.01 Bot developed by BleizeN.
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from datetime import datetime
 import discord
 from discord.ext import commands
+from matplotlib.pyplot import title
 
+# Add a prefix to command bot in Discord
+bot = commands.Bot(command_prefix='!', description="Hi all, I'm SefiBOT, your robotic friend")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-commands.Bot
+# About. Some examples
+@bot.command()
+async def about(ctx):
+    embed = discord.Embed(title = f"{ctx.guild.name}", description = "SefiBOT, developed by **BleizeN**", timestamp=datetime.utcnow(), color = discord.Color.blue())
+    embed.add_field(name="Server created by testing proposals at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server region", value=f"{ctx.guild.region}")  
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1401793142324699136/ECowi4TP_400x400.jpg")
+    await ctx.send(embed = embed)
 
-print('test')
+# Event on connect
+@bot.event
+async def on_ready():
+    # await bot.change_presence(activity="busy")
+    print("SefiBOT is connected. Prepare your materia")
+
+bot.run('<YOUR ID>')
